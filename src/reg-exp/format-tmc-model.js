e0b4b9185c4185bdf65ab4ff981ded701f358ef5
Jill Yan <yan95599@gmail.com>
2018-11-15 18:00:14 +0800

    allow @_. in json key

--- src/reg-exp/format-tmc-model.js
+++ src/reg-exp/format-tmc-model.js
@@ -1,19 +1,32 @@
-const demoData = require('./DemoService');
+const demoData = require('./metricLog');
+
 let str = JSON.stringify(demoData);
-const reg1 = /"[a-z,A-Z]+":/g;
+const reg1 = /"[0-9a-zA-Z_@.]+":/g;
 str.match(reg1).forEach(item => {
-	const rightKey = item.replace(/"/g, '');
-	str = str.replace(item, rightKey);
+	if (item.indexOf('.') > -1 || item.indexOf('@') > -1) {
+		str = str.replace(item, item.replace(/"/g, '\''));
+	} else {
+		str = str.replace(item, item.replace(/"/g, ''));
+	}
 });
+
 const reg2 = /"(.*)"/g;
-str.match(reg2).forEach(item => {
-	const rightKey = item.replace(/"/g, '\'');
-	str = str.replace(item, rightKey);
-});
+let match = str.match(reg2);
+if (match && match.length > 0) {
+	match.forEach(item => {
+		const rightKey = item.replace(/"/g, '\'');
+		str = str.replace(item, rightKey);
+	});
+}
+
 const reg3 = /'}/g;
-str.match(reg3).forEach(item => {
-	const rightKey = item.replace(/}/g, ',}');
-	str = str.replace(item, rightKey);
-});
+match = str.match(reg3);
+if (match && match.length > 0) {
+	match.forEach(item => {
+		const rightKey = item.replace(/}/g, ',}');
+		str = str.replace(item, rightKey);
+	});
+}
+
 str = `module.exports = ${str};`;
 console.log(str);
